bc24e5e4ad26e29459dec7227b554f6236ac8af2
CodingTurtle18 <105945004+CodingTurtle18@users.noreply.github.com>
2022-06-14 07:19:43 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+import List from './components/List';
+
+const App = () => {
+	return (
+		<div className='todo-app'>
+			<List />
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import Form from './Form';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+import { AiOutlineEdit } from 'react-icons/ai';
+
+const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
+	const [edit, setEdit] = useState({
+		id: null,
+		value: '',
+	});
+
+	const submitUpdate = (value) => {
+		updateTodo(edit.id, value);
+		setEdit({
+			id: null,
+			value: '',
+		});
+	};
+
+	if (edit.id) {
+		return <Form edit={edit} onSubmit={submitUpdate} />;
+	}
+
+	return todos.map((todo, index) => (
+		<div
+			className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+			key={index}>
+			<div key={todo.id} onClick={() => completeTodo(todo.id)}>
+				{todo.text}
+			</div>
+			<div className='icons'>
+				<AiOutlineCloseCircle
+					onClick={() => removeTodo(todo.id)}
+					className='delete-icon'
+				/>
+				<AiOutlineEdit
+					onClick={() => setEdit({ id: todo.id, value: todo.text })}
+					className='edit-icon'
+				/>
+			</div>
+		</div>
+	));
+};
+
+export default Todo;
